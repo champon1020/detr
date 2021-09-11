26d6539711cc27c543e21d3e850961e42e74751a
champon <nagatelu1020@gmail.com>
2021-09-11 21:38:50 +0000

    resolve Too many open files

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 import numpy as np
 import torch
+import torch.multiprocessing
 from torch.utils.data import DataLoader, DistributedSampler
 
 import datasets
@@ -25,9 +26,7 @@ def get_args_parser():
     parser.add_argument("--weight_decay", default=1e-4, type=float)
     parser.add_argument("--epochs", default=300, type=int)
     parser.add_argument("--lr_drop", default=200, type=int)
-    parser.add_argument(
-        "--clip_max_norm", default=0.1, type=float, help="gradient clipping max norm"
-    )
+    parser.add_argument("--clip_max_norm", default=0.1, type=float, help="gradient clipping max norm")
 
     # Model parameters
     parser.add_argument(
@@ -81,18 +80,14 @@ def get_args_parser():
         type=int,
         help="Size of the embeddings (dimension of the transformer)",
     )
-    parser.add_argument(
-        "--dropout", default=0.1, type=float, help="Dropout applied in the transformer"
-    )
+    parser.add_argument("--dropout", default=0.1, type=float, help="Dropout applied in the transformer")
     parser.add_argument(
         "--nheads",
         default=8,
         type=int,
         help="Number of attention heads inside the transformer's attentions",
     )
-    parser.add_argument(
-        "--num_queries", default=100, type=int, help="Number of query slots"
-    )
+    parser.add_argument("--num_queries", default=100, type=int, help="Number of query slots")
     parser.add_argument("--pre_norm", action="store_true")
 
     # * Segmentation
@@ -147,31 +142,23 @@ def get_args_parser():
     parser.add_argument("--coco_panoptic_path", type=str)
     parser.add_argument("--remove_difficult", action="store_true")
 
-    parser.add_argument(
-        "--output_dir", default="", help="path where to save, empty for no saving"
-    )
-    parser.add_argument(
-        "--device", default="cuda", help="device to use for training / testing"
-    )
+    parser.add_argument("--output_dir", default="", help="path where to save, empty for no saving")
+    parser.add_argument("--device", default="cuda", help="device to use for training / testing")
     parser.add_argument("--seed", default=42, type=int)
     parser.add_argument("--resume", default="", help="resume from checkpoint")
-    parser.add_argument(
-        "--start_epoch", default=0, type=int, metavar="N", help="start epoch"
-    )
+    parser.add_argument("--start_epoch", default=0, type=int, metavar="N", help="start epoch")
     parser.add_argument("--eval", action="store_true")
     parser.add_argument("--num_workers", default=2, type=int)
 
     # distributed training parameters
-    parser.add_argument(
-        "--world_size", default=1, type=int, help="number of distributed processes"
-    )
-    parser.add_argument(
-        "--dist_url", default="env://", help="url used to set up distributed training"
-    )
+    parser.add_argument("--world_size", default=1, type=int, help="number of distributed processes")
+    parser.add_argument("--dist_url", default="env://", help="url used to set up distributed training")
     return parser
 
 
 def main(args):
+    torch.multiprocessing.set_sharing_strategy("file_system")
+
     utils.init_distributed_mode(args)
     print("git:\n  {}\n".format(utils.get_sha()))
 
@@ -198,25 +185,13 @@ def main(args):
     print("number of params:", n_parameters)
 
     param_dicts = [
+        {"params": [p for n, p in model_without_ddp.named_parameters() if "backbone" not in n and p.requires_grad]},
         {
-            "params": [
-                p
-                for n, p in model_without_ddp.named_parameters()
-                if "backbone" not in n and p.requires_grad
-            ]
-        },
-        {
-            "params": [
-                p
-                for n, p in model_without_ddp.named_parameters()
-                if "backbone" in n and p.requires_grad
-            ],
+            "params": [p for n, p in model_without_ddp.named_parameters() if "backbone" in n and p.requires_grad],
             "lr": args.lr_backbone,
         },
     ]
-    optimizer = torch.optim.AdamW(
-        param_dicts, lr=args.lr, weight_decay=args.weight_decay
-    )
+    optimizer = torch.optim.AdamW(param_dicts, lr=args.lr, weight_decay=args.weight_decay)
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, args.lr_drop)
 
     dataset_train = build_dataset(image_set="train", args=args)
@@ -229,9 +204,7 @@ def main(args):
         sampler_train = torch.utils.data.RandomSampler(dataset_train)
         sampler_val = torch.utils.data.SequentialSampler(dataset_val)
 
-    batch_sampler_train = torch.utils.data.BatchSampler(
-        sampler_train, args.batch_size, drop_last=True
-    )
+    batch_sampler_train = torch.utils.data.BatchSampler(sampler_train, args.batch_size, drop_last=True)
 
     data_loader_train = DataLoader(
         dataset_train,
@@ -262,18 +235,11 @@ def main(args):
     output_dir = Path(args.output_dir)
     if args.resume:
         if args.resume.startswith("https"):
-            checkpoint = torch.hub.load_state_dict_from_url(
-                args.resume, map_location="cpu", check_hash=True
-            )
+            checkpoint = torch.hub.load_state_dict_from_url(args.resume, map_location="cpu", check_hash=True)
         else:
             checkpoint = torch.load(args.resume, map_location="cpu")
         model_without_ddp.load_state_dict(checkpoint["model"])
-        if (
-            not args.eval
-            and "optimizer" in checkpoint
-            and "lr_scheduler" in checkpoint
-            and "epoch" in checkpoint
-        ):
+        if not args.eval and "optimizer" in checkpoint and "lr_scheduler" in checkpoint and "epoch" in checkpoint:
             optimizer.load_state_dict(checkpoint["optimizer"])
             lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
             args.start_epoch = checkpoint["epoch"] + 1
@@ -289,9 +255,7 @@ def main(args):
             args.output_dir,
         )
         if args.output_dir:
-            utils.save_on_master(
-                coco_evaluator.coco_eval["bbox"].eval, output_dir / "eval.pth"
-            )
+            utils.save_on_master(coco_evaluator.coco_eval["bbox"].eval, output_dir / "eval.pth")
         return
 
     print("Start training")
@@ -366,9 +330,7 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        "DETR training and evaluation script", parents=[get_args_parser()]
-    )
+    parser = argparse.ArgumentParser("DETR training and evaluation script", parents=[get_args_parser()])
     args = parser.parse_args()
     if args.output_dir:
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
